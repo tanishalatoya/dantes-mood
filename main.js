f2090bc709f74f1c26bb7aaf783bfbdd2514b625
Tanisha L Davey <tanisha.davey2@gmail.com>
2021-03-04 19:34:57 -0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,44 @@
+import { allMoodMessages } from './data';
+
+document.addEventListener('load', () => {
+    const allMoodsDiv = document.querySelector(".buttons-all-moods");
+    const clearButton = document.querySelector("#clear");
+    const messageDisplay = document.querySelector(".section-message-display");
+    const clearPreviousMsg = () => {
+        messageDisplay.innerHTML = '';
+    }
+
+    clearButton.disabled = true;
+    clearPreviousMsg();
+
+    function displayMessage(e) {
+        const moodTypes = Object.keys(allMoodMessages);
+        const selectedMood = () => {
+            return moodTypes.find(type => {
+                return type === e.target.id
+            })
+        }
+        const randomNumber = Math.floor(Math.random() * selectedMood().length);
+
+      if (e.target.id === selectedMood()) {
+       clearPreviousMsg()
+       clearButton.disabled = false;
+        messageDisplay.insertAdjacentHTML(
+          "afterbegin",
+          `<div>${allMoodMessages[selectedMood()][randomNumber]}</div>`
+        );
+      }
+    }
+
+    function clearMessages() {
+        if (messageDisplay.innerHTML !== '') {
+            clearButton.disabled = true;
+            clearPreviousMsg();
+            clearButton.addClassList('change-to-default-color');
+        }
+    }
+    
+    allMoodsDiv.addEventListener("click", displayMessage);
+    clearButton.addEventListener('click', clearMessages)
+
+}, true);
